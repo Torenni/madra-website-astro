a19f0077b8928a955ea2fa94110a39c4289d9cf6
Wesseh <yarlhaven@gmail.com>
2025-12-20 21:51:53 -0500

    Update astro.config.mjs

--- intergalactic-inclination/astro.config.mjs
+++ intergalactic-inclination/astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://torenni.github.io",
-  base: "/madra-website-astro",
+  integrations: [tailwind()],
 });
